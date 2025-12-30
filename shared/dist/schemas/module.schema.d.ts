845a632ba0f92fb3a0351cc48a1010a0aecf721d
Iorie Chua <ioriecodez@gmail.com>
2025-12-30 15:01:44 +0800

    chore: remove dist folder and tsbuildinfo

--- shared/dist/schemas/module.schema.d.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-export declare const moduleSchemaZod: z.ZodObject<{
-    title: z.ZodString;
-    description: z.ZodString;
-    contentUrl: z.ZodString;
-    createdAt: z.ZodDefault<z.ZodDate>;
-}, z.core.$strip>;
-export type ModuleInput = z.infer<typeof moduleSchemaZod>;
