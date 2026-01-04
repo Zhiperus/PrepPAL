e9235cf2337c3e1e7be515dd743800c300268b05
jarencerivera00 <jcrivera12@up.edu.ph>
2026-01-04 08:53:01 +0000

    fix: exclude content in getAllModules

--- backend/src/repositories/module.repository.ts
+++ backend/src/repositories/module.repository.ts
@@ -2,7 +2,7 @@ import Module from '../models/module.model.js';
 
 export default class ModuleRepository {
   async getAll() {
-    return Module.find();
+    return Module.find().select('-content');
   }
 
   async getModule(id: string) {
