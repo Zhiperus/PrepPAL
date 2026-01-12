831fc9b6dd2df7f5ebab0f7c3a2e94486624efcc
jarencerivera00 <jcrivera12@up.edu.ph>
2026-01-12 13:46:35 +0000

    fix: renamed questionReport model fields to match the schema

--- backend/src/models/questionReport.model.ts
+++ backend/src/models/questionReport.model.ts
@@ -2,20 +2,20 @@ import { QuestionReport } from '@repo/shared/dist/schemas/questionReport.schema'
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IQuestionReport
-  extends Omit<QuestionReport, 'id' | 'lguId' | 'postId' | 'reporterId'>,
+  extends Omit<QuestionReport, 'id' | 'quizId' | 'questionId' | 'reporterId'>,
     Document {
-  postId: mongoose.Types.ObjectId;
+  quizId: mongoose.Types.ObjectId;
+  questionId: mongoose.Types.ObjectId;
   reporterId: mongoose.Types.ObjectId;
-  lguId: mongoose.Types.ObjectId;
   reason: string;
   status: 'PENDING' | 'RESOLVED' | 'DISMISSED';
 }
 
 const questionReportSchema = new Schema<IQuestionReport>(
   {
-    postId: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+    quizId: { type: Schema.Types.ObjectId, ref: 'Quiz', required: true },
+    questionId: { type: Schema.Types.ObjectId, required: true },
     reporterId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    lguId: { type: Schema.Types.ObjectId, ref: 'Lgu', required: true },
     reason: { type: String, required: true },
     status: {
       type: String,
